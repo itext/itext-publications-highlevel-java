b45e4bb9b3fedd57aa1d726b51ef2a0331c83345
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2016-05-20 11:38:24 +0200

    Adding a first draft of a new set of examples

---
+++ src/main/java/com/itextpdf/highlevel/chapter01/JekyllHydeV1.java
@@ -0,0 +1,51 @@
+/*
+ * This example was written by Bruno Lowagie
+ * in the context of the book: iText 7 layout objects
+ */
+package com.itextpdf.highlevel.chapter01;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * @author Bruno Lowagie (iText Software)
+ */
+public class JekyllHydeV1 {
+    public static final String SRC = "src/main/resources/txt/jekyll_hyde.txt";
+    public static final String DEST = "results/chapter01/jekyll_hyde_v1.pdf";
+    
+    public static void main(String args[]) throws IOException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new JekyllHydeV1().createPdf(DEST);
+    }
+    
+    public void createPdf(String dest) throws IOException {
+        //Initialize PDF writer
+        FileOutputStream fos = new FileOutputStream(dest);
+        PdfWriter writer = new PdfWriter(fos);
+
+        //Initialize PDF document
+        PdfDocument pdf = new PdfDocument(writer);
+        
+        // Initialize document
+        Document document = new Document(pdf);
+
+        BufferedReader br = new BufferedReader(new FileReader(SRC));
+        String line;
+        while ((line = br.readLine()) != null) {
+            document.add(new Paragraph(line));
+        }
+
+        //Close document
+        document.close();
+    }
+
+}
---
+++ src/main/java/com/itextpdf/highlevel/chapter01/JekyllHydeV2.java
@@ -0,0 +1,55 @@
+/*
+ * This example was written by Bruno Lowagie
+ * in the context of the book: iText 7 layout objects
+ */
+package com.itextpdf.highlevel.chapter01;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.hyphenation.HyphenationConfig;
+import com.itextpdf.layout.property.TextAlignment;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * @author Bruno Lowagie (iText Software)
+ */
+public class JekyllHydeV2 {
+    public static final String SRC = "src/main/resources/txt/jekyll_hyde.txt";
+    public static final String DEST = "results/chapter01/jekyll_hyde_v2.pdf";
+    
+    public static void main(String args[]) throws IOException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new JekyllHydeV2().createPdf(DEST);
+    }
+    
+    public void createPdf(String dest) throws IOException {
+        //Initialize PDF writer
+        FileOutputStream fos = new FileOutputStream(dest);
+        PdfWriter writer = new PdfWriter(fos);
+
+        //Initialize PDF document
+        PdfDocument pdf = new PdfDocument(writer);
+        
+        // Initialize document
+        Document document = new Document(pdf);
+        document.setTextAlignment(TextAlignment.JUSTIFIED)
+            .setHyphenation(new HyphenationConfig("en", "uk", 3, 3));
+        
+        BufferedReader br = new BufferedReader(new FileReader(SRC));
+        String line;
+        while ((line = br.readLine()) != null) {
+            document.add(new Paragraph(line));
+        }
+
+        //Close document
+        document.close();
+    }
+
+}
