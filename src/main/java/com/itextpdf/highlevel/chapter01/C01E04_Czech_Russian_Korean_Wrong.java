5940b37fbe40f321fcd1f28c12b2a42b70a5bfa5
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2016-06-01 11:37:07 +0200

    New examples building blocks tutorial

---
+++ src/main/java/com/itextpdf/highlevel/chapter01/C01E04_Czech_Russian_Korean_Wrong.java
@@ -0,0 +1,65 @@
+/*
+ * This example was written by Bruno Lowagie
+ * in the context of the book: iText 7 layout objects
+ */
+package com.itextpdf.highlevel.chapter01;
+
+import com.itextpdf.io.font.FontConstants;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * @author Bruno Lowagie (iText Software)
+ */
+public class C01E04_Czech_Russian_Korean_Wrong {
+    
+    public static final String DEST = "results/chapter01/czech_russian_korean_wrong.pdf";
+    
+    public static final String CZECH =
+            "schizofrenik s jednou dobrou a jednou \u0161patnou osobnost\u00ed";
+    public static final String RUSSIAN =
+            "\u0421\u0442\u0440\u0430\u043d\u043d\u0430\u044f "
+            + "\u0438\u0441\u0442\u043e\u0440\u0438\u044f "
+            + "\u0434\u043e\u043a\u0442\u043e\u0440\u0430 "
+            + "\u0414\u0436\u0435\u043a\u0438\u043b\u0430 \u0438 "
+            + "\u043c\u0438\u0441\u0442\u0435\u0440\u0430 "
+            + "\u0425\u0430\u0439\u0434\u0430";
+    public static final String KOREAN =
+            "\ud558\uc774\ub4dc, \uc9c0\ud0ac, \ub098";
+    
+    public static void main(String args[]) throws IOException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new C01E04_Czech_Russian_Korean_Wrong().createPdf(DEST);
+    }
+    
+    public void createPdf(String dest) throws IOException {
+        // Initialize PDF writer and PDF document
+        OutputStream fos = new FileOutputStream(dest);
+        PdfWriter writer = new PdfWriter(fos);
+        PdfDocument pdf = new PdfDocument(writer);
+        
+        // Initialize document
+        Document document = new Document(pdf);
+        
+        // Add content
+        PdfFont font = PdfFontFactory.createFont(FontConstants.TIMES_ROMAN);
+        document.add(new Paragraph().setFont(font)
+                .add(CZECH).add(" by Robert Louis Stevenson"));
+        document.add(new Paragraph().setFont(font)
+                .add(RUSSIAN).add(" by Robert Louis Stevenson"));
+        document.add(new Paragraph().setFont(font)
+                .add(KOREAN).add(" by Robert Louis Stevenson"));
+        
+        //Close document
+        document.close();
+    }
+}
