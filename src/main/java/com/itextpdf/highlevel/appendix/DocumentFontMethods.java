bb53f07e0234ed50414e9025f93d047ca54bf830
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2016-06-04 19:31:06 +0200

    Test examples for the appendix

---
+++ src/main/java/com/itextpdf/highlevel/appendix/DocumentFontMethods.java
@@ -0,0 +1,91 @@
+/*
+ * This example was written by Bruno Lowagie
+ * in the context of the book: iText 7 layout objects
+ */
+package com.itextpdf.highlevel.appendix;
+
+import com.itextpdf.io.font.FontConstants;
+import com.itextpdf.kernel.color.Color;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvasConstants;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.property.FontKerning;
+import com.itextpdf.layout.property.Property;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * @author Bruno Lowagie (iText Software)
+ */
+public class DocumentFontMethods {
+    
+    public static final String DEST = "results/appendix/document_font_methods.pdf";
+    
+    public static void main(String args[]) throws IOException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new DocumentFontMethods().createPdf(DEST);
+    }
+    
+    public void createPdf(String dest) throws IOException {
+        // Initialize PDF writer and PDF document
+        OutputStream fos = new FileOutputStream(dest);
+        PdfWriter writer = new PdfWriter(fos);
+        PdfDocument pdf = new PdfDocument(writer);
+        
+        // Initialize document
+        Document document = new Document(pdf);
+        Paragraph p;
+        p = new Paragraph("Testing font methods");
+        document.add(p);
+        PdfFont font = PdfFontFactory.createFont(FontConstants.TIMES_ROMAN);
+        document.setFont(font);
+        p = new Paragraph("Testing font methods: changed font");
+        document.add(p);
+        document.setFontSize(18);
+        p = new Paragraph("Testing font methods: changed font size");
+        document.add(p);
+        document.setFontColor(Color.BLUE);
+        p = new Paragraph("Testing font methods: changed color");
+        document.add(p);
+        document.setBold();
+        p = new Paragraph("Testing font methods: to bold");
+        document.add(p);
+        document.setItalic();
+        p = new Paragraph("Testing font methods: to italic");
+        document.add(p);
+        document.setProperty(Property.BOLD_SIMULATION, false);
+        document.setProperty(Property.ITALIC_SIMULATION, false);
+        document.setProperty(Property.FONT_COLOR, null);
+        p = new Paragraph("Testing font methods: resetting style and color");
+        document.add(p);
+        document.setLineThrough();
+        p = new Paragraph("Testing font methods: line through (default)");
+        document.add(p);
+        document.setProperty(Property.UNDERLINE, null);
+        document.setUnderline();
+        p = new Paragraph("Testing font methods: underline (default)");
+        document.add(p);
+        document.setProperty(Property.UNDERLINE, null);
+        document.setUnderline(2, 4);
+        document.setUnderline(Color.BLUE, 5, 0.1f, 2, -0.5f, PdfCanvasConstants.LineCapStyle.ROUND);
+        p = new Paragraph("Testing font methods: underline (custom)");
+        document.add(p);
+        document.setProperty(Property.UNDERLINE, null);
+        document.setTextRenderingMode(PdfCanvasConstants.TextRenderingMode.STROKE);
+        p = new Paragraph("Testing font methods: change text rendering mode");
+        document.add(p);
+        document.setStrokeWidth(0.1f);
+        document.setStrokeColor(Color.BLUE);
+        p = new Paragraph("Testing font methods: change stroke width and color");
+        document.add(p);
+        //Close document
+        document.close();
+    }
+}
---
+++ src/main/java/com/itextpdf/highlevel/appendix/DocumentLayoutMethods.java
@@ -0,0 +1,91 @@
+/*
+ * This example was written by Bruno Lowagie
+ * in the context of the book: iText 7 layout objects
+ */
+package com.itextpdf.highlevel.appendix;
+
+import com.itextpdf.io.font.FontConstants;
+import com.itextpdf.io.font.otf.GlyphLine;
+import com.itextpdf.kernel.color.Color;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvasConstants;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.border.Border;
+import com.itextpdf.layout.border.SolidBorder;
+import com.itextpdf.layout.element.AreaBreak;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.hyphenation.HyphenationConfig;
+import com.itextpdf.layout.property.HorizontalAlignment;
+import com.itextpdf.layout.property.Property;
+import com.itextpdf.layout.property.TextAlignment;
+import com.itextpdf.layout.splitting.DefaultSplitCharacters;
+import com.itextpdf.layout.splitting.ISplitCharacters;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * @author Bruno Lowagie (iText Software)
+ */
+public class DocumentLayoutMethods {
+    
+    public static final String DEST = "results/appendix/document_layout_methods.pdf";
+    
+    public static void main(String args[]) throws IOException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new DocumentLayoutMethods().createPdf(DEST);
+    }
+    
+    public void createPdf(String dest) throws IOException {
+        // Initialize PDF writer and PDF document
+        OutputStream fos = new FileOutputStream(dest);
+        PdfWriter writer = new PdfWriter(fos);
+        PdfDocument pdf = new PdfDocument(writer);
+        
+        // Initialize document
+        Document document = new Document(pdf);
+        document.setHeight(300);
+        document.setWidth(300);
+        Paragraph p;
+        p = new Paragraph("Testing layout methods");
+        document.add(p);
+        document.setTextAlignment(TextAlignment.CENTER);
+        p = new Paragraph("Testing layout methods");
+        document.add(p);
+        p = new Paragraph();
+        for (int i = 0; i < 10; i++)
+            p.add("singing supercalifragilisticexpialidocious ");
+        document.add(p);
+        document.setHyphenation(new HyphenationConfig("en", "uk", 3, 3));
+        document.add(p);
+        document.setTextAlignment(TextAlignment.JUSTIFIED);
+        document.add(p);
+        document.setHyphenation(null);
+        document.setSplitCharacters(new ISplitCharacters() {
+            public boolean isSplitCharacter(GlyphLine text, int glyphPos) {
+                if (!text.get(glyphPos).hasValidUnicode()) {
+                    return false;
+                }
+                int charCode = (int)text.get(glyphPos).getUnicode();
+                return (charCode < ' ' || charCode == 'i');
+            }
+            
+        });
+        document.add(p);
+        document.setSplitCharacters(new DefaultSplitCharacters());
+        document.setTextAlignment(TextAlignment.LEFT);
+        document.add(p);
+        document.setWordSpacing(10);
+        document.add(p);
+        document.setCharacterSpacing(5);
+        document.add(p);
+        //Close document
+        document.close();
+    }
+}
