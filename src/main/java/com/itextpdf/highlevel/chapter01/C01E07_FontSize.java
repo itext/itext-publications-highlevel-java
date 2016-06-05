bded755e6e2be0228e9b58cfe143912e0b033acb
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2016-06-05 15:44:50 +0200

    New examples for building blocks tutorial chapter 1

---
+++ src/main/java/com/itextpdf/highlevel/chapter01/C01E07_FontSize.java
@@ -0,0 +1,50 @@
+/*
+ * This example was written by Bruno Lowagie
+ * in the context of the book: iText 7 layout objects
+ */
+package com.itextpdf.highlevel.chapter01;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Text;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * @author Bruno Lowagie (iText Software)
+ */
+public class C01E07_FontSize {
+    
+    public static final String DEST = "results/chapter01/font_size.pdf";
+    
+    public static void main(String args[]) throws IOException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new C01E07_FontSize().createPdf(DEST);
+    }
+    
+    public void createPdf(String dest) throws IOException {
+        // Initialize PDF writer and PDF document
+        OutputStream fos = new FileOutputStream(dest);
+        PdfWriter writer = new PdfWriter(fos);
+        PdfDocument pdf = new PdfDocument(writer);
+        
+        // Initialize document
+        Document document = new Document(pdf);
+        
+        // Add content
+        Text title1 = new Text("The Strange Case of ").setFontSize(12);
+        Text title2 = new Text("Dr. Jekyll and Mr. Hyde").setFontSize(16);
+        Text author = new Text("Robert Louis Stevenson");
+        Paragraph p = new Paragraph().setFontSize(8)
+                .add(title1).add(title2).add(" by ").add(author);
+        document.add(p);
+        
+        //Close document
+        document.close();
+    }
+}
---
+++ src/main/java/com/itextpdf/highlevel/chapter01/C01E09_ColorRendering.java
@@ -0,0 +1,57 @@
+/*
+ * This example was written by Bruno Lowagie
+ * in the context of the book: iText 7 layout objects
+ */
+package com.itextpdf.highlevel.chapter01;
+
+import com.itextpdf.kernel.color.Color;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvasConstants;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Text;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * @author Bruno Lowagie (iText Software)
+ */
+public class C01E09_ColorRendering {
+    
+    public static final String DEST = "results/chapter01/color_rendermode.pdf";
+    
+    public static void main(String args[]) throws IOException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new C01E09_ColorRendering().createPdf(DEST);
+    }
+    
+    public void createPdf(String dest) throws IOException {
+        // Initialize PDF writer and PDF document
+        OutputStream fos = new FileOutputStream(dest);
+        PdfWriter writer = new PdfWriter(fos);
+        PdfDocument pdf = new PdfDocument(writer);
+        
+        // Initialize document
+        Document document = new Document(pdf);
+        
+        // Add content
+        Text title1 = new Text("The Strange Case of ").setFontColor(Color.BLUE);
+        Text title2 = new Text("Dr. Jekyll")
+                .setStrokeColor(Color.GREEN)
+                .setTextRenderingMode(PdfCanvasConstants.TextRenderingMode.FILL_STROKE);
+        Text title3 = new Text(" and ");
+        Text title4 = new Text("Mr. Hyde")
+                .setStrokeColor(Color.RED).setStrokeWidth(0.5f)
+                .setTextRenderingMode(PdfCanvasConstants.TextRenderingMode.STROKE);
+        Paragraph p = new Paragraph().setFontSize(24)
+                .add(title1).add(title2).add(title3).add(title4);
+        document.add(p);
+        
+        //Close document
+        document.close();
+    }
+}
