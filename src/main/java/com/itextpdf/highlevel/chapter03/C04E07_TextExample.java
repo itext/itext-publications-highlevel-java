a68e87bd877b0ef952d3b0ff5aa222b0b6e991e7
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2016-06-21 11:35:52 +0200

    New example for chapter 3 iText 7 building blocks

---
+++ src/main/java/com/itextpdf/highlevel/chapter03/C04E07_TextExample.java
@@ -0,0 +1,37 @@
+/*
+ * This example was written by Bruno Lowagie
+ * in the context of the book: iText 7 layout objects
+ */
+package com.itextpdf.highlevel.chapter03;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Text;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class C04E07_TextExample {
+    
+    public static final String DEST = "results/chapter03/jekyll_hyde_text.pdf";
+       
+    public static void main(String args[]) throws IOException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new C04E07_TextExample().createPdf(DEST);
+    }
+    
+    public void createPdf(String dest) throws IOException {
+        PdfDocument pdf = new PdfDocument(
+            new PdfWriter(new FileOutputStream(dest)));
+        Document document = new Document(pdf);
+        Text t1 = new Text("The Strange Case of ");
+        Text t2 = new Text("Dr. Jekyll").setTextRise(5);
+        Text t3 = new Text(" and ").setHorizontalScaling(2);
+        Text t4 = new Text("Mr. Hyde").setSkew(10, 45);
+        document.add(new Paragraph(t1).add(t2).add(t3).add(t4));
+        document.close();
+    }
+}
