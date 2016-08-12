5b64bd3b0f1fcb8f559d751039917ce2779600b4
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2016-08-12 14:48:11 +0200

    new examples for chapter 6

---
+++ src/main/java/com/itextpdf/highlevel/chapter06/C06E12_Outlines.java
@@ -0,0 +1,69 @@
+/*
+ * This example was written by Bruno Lowagie
+ * in the context of the book: iText 7 layout objects
+ */
+package com.itextpdf.highlevel.chapter06;
+
+import static com.itextpdf.highlevel.chapter06.C06E01_URIAction.SRC;
+import com.itextpdf.highlevel.util.CsvTo2DList;
+import com.itextpdf.io.image.ImageDataFactory;
+import com.itextpdf.kernel.color.Color;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfName;
+import com.itextpdf.kernel.pdf.PdfOutline;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.action.PdfAction;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Image;
+import com.itextpdf.layout.element.ListItem;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.property.ListNumberingType;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author Bruno Lowagie (iText Software)
+ */
+public class C06E12_Outlines {
+    
+    public static final String SRC = "src/main/resources/data/jekyll_hyde.csv";
+    public static final String DEST = "results/chapter06/jekyll_hyde_outlines.pdf";
+       
+    public static void main(String args[]) throws IOException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new C06E12_Outlines().createPdf(DEST);
+    }
+    
+    public void createPdf(String dest) throws IOException {
+        PdfDocument pdf = new PdfDocument(new PdfWriter(dest));
+        PdfPage page = pdf.addNewPage();
+        pdf.getCatalog().setPageMode(PdfName.UseOutlines);
+        PdfOutline root = pdf.getOutlines(false);
+        List<List<String>> resultSet = CsvTo2DList.convert(SRC, "|");
+        resultSet.remove(0);
+        for (List<String> record : resultSet) {
+            PdfOutline movie = root.addOutline(record.get(2));
+            PdfOutline imdb = movie.addOutline("Link to IMDB");
+            imdb.setColor(Color.BLUE);
+            imdb.setStyle(PdfOutline.FLAG_BOLD);
+            String url = String.format(
+                "http://www.imdb.com/title/tt%s", record.get(0));
+            imdb.addAction(PdfAction.createURI(url));
+            PdfOutline info = movie.addOutline("More info:");
+            info.setOpen(false);
+            info.setStyle(PdfOutline.FLAG_ITALIC);
+            PdfOutline director = info.addOutline("Directed by " + record.get(3));
+            director.setColor(Color.RED);
+            PdfOutline place = info.addOutline("Produced in " + record.get(4));
+            place.setColor(Color.MAGENTA);
+            PdfOutline year = info.addOutline("Released in " + record.get(5));
+            year.setColor(Color.DARK_GRAY);
+        }
+        pdf.close();
+    }
+    
+}
