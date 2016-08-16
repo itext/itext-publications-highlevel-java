5c1eb2176569925f3b0e1a6d74483fc2eb4d3796
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2016-08-16 17:10:14 +0200

    Replace AreaBreakType.LAST_PAGE example with an example that explains how to switch renderers

--- src/main/java/com/itextpdf/highlevel/chapter02/C02E10_JekyllHydeV6.java
+++ src/main/java/com/itextpdf/highlevel/chapter02/C02E10_JekyllHydeV6.java
@@ -4,15 +4,25 @@
  */
 package com.itextpdf.highlevel.chapter02;
 
+import com.itextpdf.io.font.FontConstants;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.geom.Rectangle;
 import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.ColumnDocumentRenderer;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.AreaBreak;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.hyphenation.HyphenationConfig;
 import com.itextpdf.layout.property.AreaBreakType;
+import com.itextpdf.layout.property.TextAlignment;
+import com.itextpdf.layout.renderer.DocumentRenderer;
 import com.itextpdf.test.annotations.WrapToTest;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 /**
@@ -20,32 +30,81 @@ import java.io.IOException;
  */
 @WrapToTest
 public class C02E10_JekyllHydeV6 {
-    public static final String SRC = "src/main/resources/pdfs/jekyll_hyde.pdf";
+    public static final String SRC = "src/main/resources/txt/jekyll_hyde.txt";
     public static final String DEST = "results/chapter02/jekyll_hyde_v6.pdf";
     
     public static void main(String args[]) throws IOException {
         File file = new File(DEST);
         file.getParentFile().mkdirs();
-        new C02E10_JekyllHydeV6().manipulatePdf(SRC, DEST);
+        new C02E10_JekyllHydeV6().createPdf(DEST);
     }
     
-    public void manipulatePdf(String src, String dest) throws IOException {
-
-        //Initialize PDF reader
-        PdfReader reader = new PdfReader(src);
-
-        //Initialize PDF writer
-        PdfWriter writer = new PdfWriter(dest);
-
+    public void createPdf(String dest) throws IOException {
         //Initialize PDF document
-        PdfDocument pdfDoc = new PdfDocument(reader, writer);
-        Document document = new Document(pdfDoc);
-        document.setTopMargin(10);
-        document.add(new Paragraph("This is the first page!"));
-        document.add(new AreaBreak(AreaBreakType.LAST_PAGE));
-        document.add(new Paragraph("This is the last page!"));
-        document.close();
+        PdfDocument pdf = new PdfDocument(new PdfWriter(dest));
+        
+        // Initialize document
+        Document document = new Document(pdf);
+        Paragraph p = new Paragraph()
+            .add("Be prepared to read a story about a London lawyer "
+            + "named Gabriel John Utterson who investigates strange "
+            + "occurrences between his old friend, Dr. Henry Jekyll, "
+            + "and the evil Edward Hyde.");
+        document.add(p);
+        document.add(new AreaBreak(AreaBreakType.NEXT_PAGE));
+        
+        //Set column parameters
+        float offSet = 36;
+        float gutter = 23;
+        float columnWidth = (PageSize.A4.getWidth() - offSet * 2) / 2 - gutter;
+        float columnHeight = PageSize.A4.getHeight() - offSet * 2;
 
+        //Define column areas
+        Rectangle[] columns = {
+            new Rectangle(offSet, offSet, columnWidth, columnHeight),
+            new Rectangle(offSet + columnWidth + gutter, offSet, columnWidth, columnHeight)};
+        document.setRenderer(new ColumnDocumentRenderer(document, columns));
+        document.add(new AreaBreak(AreaBreakType.LAST_PAGE));   
+        
+        PdfFont font = PdfFontFactory.createFont(FontConstants.TIMES_ROMAN);
+        PdfFont bold = PdfFontFactory.createFont(FontConstants.HELVETICA_BOLD);
+        document.setTextAlignment(TextAlignment.JUSTIFIED)
+            .setFont(font)
+            .setHyphenation(new HyphenationConfig("en", "uk", 3, 3));
+        
+        BufferedReader br = new BufferedReader(new FileReader(SRC));
+        String line;
+        boolean title = true;
+        AreaBreak nextPage = new AreaBreak(AreaBreakType.NEXT_AREA);
+        while ((line = br.readLine()) != null) {
+            p = new Paragraph(line);
+            if (title) {
+                p.setFont(bold).setFontSize(12);
+                title = false;
+            }
+            else {
+                p.setFirstLineIndent(36);
+            }
+            if (line.isEmpty()) {
+                document.add(nextPage);
+                title = true;
+            }
+            document.add(p);
+        }
+
+        document.add(new AreaBreak(AreaBreakType.NEXT_PAGE));
+        document.setRenderer(new DocumentRenderer(document)); 
+        document.add(new AreaBreak(AreaBreakType.LAST_PAGE));
+        p = new Paragraph()
+            .add("This was the story about the London lawyer "
+            + "named Gabriel John Utterson who investigates strange "
+            + "occurrences between his old friend, Dr. Henry Jekyll, "
+            + "and the evil Edward Hyde. THE END!");
+        document.add(p);
+        
+        //Close document
+        document.close();
+        
     }
 
 }
---
+++ src/main/java/com/itextpdf/highlevel/notused/Renderers.java
@@ -0,0 +1,78 @@
+/*
+ * This example was written by Bruno Lowagie
+ * in the context of the book: iText 7 layout objects
+ */
+package com.itextpdf.highlevel.notused;
+
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.ColumnDocumentRenderer;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.AreaBreak;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.property.AreaBreakType;
+import com.itextpdf.layout.renderer.DocumentRenderer;
+import com.itextpdf.test.annotations.WrapToTest;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author Bruno Lowagie (iText Software)
+ */
+@WrapToTest
+public class Renderers {
+    public static final String DEST = "results/notused/renderers.pdf";
+    public static final String TEXT =
+        "This is a long sentence. We will render this paragraph in different "
+        + "ways. We'll use the default renderer as well as a column renderer. "
+        + "This is some more text to make the paragraph longer. We want it "
+        + "to span multiple lines. An example with a short paragraph isn't "
+        + "useful for us.";
+    
+    public static void main(String args[]) throws IOException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new Renderers().createPdf(DEST);
+    }
+    
+    public void createPdf(String dest) throws IOException {
+        //Initialize PDF document
+        PdfDocument pdf = new PdfDocument(new PdfWriter(dest));
+        
+        // Initialize document
+        Document document = new Document(pdf);
+        
+        Paragraph p = new Paragraph().add(TEXT);
+        document.add(p);
+        document.add(new AreaBreak(AreaBreakType.NEXT_PAGE));
+        
+        //Set column parameters
+        float offSet = 36;
+        float gutter = 23;
+        float columnWidth = (PageSize.A4.getWidth() - offSet * 2) / 2 - gutter;
+        float columnHeight = PageSize.A4.getHeight() - offSet * 2;
+
+        //Define column areas
+        Rectangle[] columns = {
+            new Rectangle(offSet, offSet, columnWidth, columnHeight),
+            new Rectangle(offSet + columnWidth + gutter, offSet, columnWidth, columnHeight)};
+        document.setRenderer(new ColumnDocumentRenderer(document, columns)); 
+        document.add(new AreaBreak(AreaBreakType.LAST_PAGE));
+
+        for (int i = 0; i < 10; i++)
+            document.add(p);
+        document.add(new AreaBreak(AreaBreakType.NEXT_PAGE));
+        
+        document.setRenderer(new DocumentRenderer(document)); 
+        document.add(new AreaBreak(AreaBreakType.LAST_PAGE));
+        document.add(p);
+        
+        //Close document
+        document.close();
+        
+    }
+
+}
