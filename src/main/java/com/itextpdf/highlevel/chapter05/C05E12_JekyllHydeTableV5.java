1c4fc740f6e7fc7be218c31c17fe65824f741cae
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2017-09-15 16:12:42 +0200

    Normalization of some building blocks chapter 5 examples

--- src/main/java/com/itextpdf/highlevel/chapter05/C05E12_JekyllHydeTableV5.java
+++ src/main/java/com/itextpdf/highlevel/chapter05/C05E12_JekyllHydeTableV5.java
@@ -48,7 +48,8 @@ public class C05E12_JekyllHydeTableV5 {
         
         // Initialize document
         Document document = new Document(pdf, PageSize.A4.rotate());
-        Table table = new Table(UnitValue.createPercentArray(new float[]{3, 32})).useAllAvailableWidth();
+        Table table = new Table(UnitValue.createPercentArray(new float[]{3, 32}));
+        table.setWidthPercent(100);
         List<List<String>> resultSet = CsvTo2DList.convert(SRC, "|");
         resultSet.remove(0);
         table.addHeaderCell("imdb")
--- src/main/java/com/itextpdf/highlevel/chapter05/C05E14_JekyllHydeTableV7.java
+++ src/main/java/com/itextpdf/highlevel/chapter05/C05E14_JekyllHydeTableV7.java
@@ -52,7 +52,8 @@ public class C05E14_JekyllHydeTableV7 {
         
         // Initialize document
         Document document = new Document(pdf, PageSize.A4.rotate());
-        Table table = new Table(UnitValue.createPercentArray(new float[]{3, 2, 14, 9, 4, 3})).useAllAvailableWidth();
+        Table table = new Table(UnitValue.createPercentArray(new float[]{3, 2, 14, 9, 4, 3}));
+        table.setWidthPercent(100);
         List<List<String>> resultSet = CsvTo2DList.convert(SRC, "|");
         List<String> header = resultSet.remove(0);
         for (String field : header) {
